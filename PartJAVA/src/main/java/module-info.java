a8ad544e7fa029c0e68afa3997d6ef83c3651216
JH Bae <128088756+JeongHan-Bae@users.noreply.github.com>
2024-01-12 00:01:46 +0100

    Update Module-Info

--- PartJAVA/src/main/java/module-info.java
+++ PartJAVA/src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.example.partjava {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.testng;
+    requires org.junit.jupiter.api;
 
 
     opens com.example.partjava to javafx.fxml;
     exports com.example.partjava;
+    exports DataTests;
 }
