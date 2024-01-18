d19af38c3782feff5e69644cf146c3483f0a81b9
JH Bae <mastropseudo@gmail.com>
2024-01-18 17:52:45 +0100

    get back the good versions

--- PartJAVA/src/main/java/com/example/partjava/CryptoOrderController.java
+++ PartJAVA/src/main/java/com/example/partjava/CryptoOrderController.java
@@ -1,5 +1,4 @@
 package com.example.partjava;
-
 import Data.GlobalObj;
 import Data.UserObj;
 import Tools.JavaClient;
@@ -8,23 +7,18 @@ import Tools.ShowAlert;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-
 public class CryptoOrderController {
     // class to put purchase or sale order and register the money or the crypto-coins
-
     @FXML
     public TextField amountTextField;
-
     @FXML
     private PasswordField passwordField;
-
     @FXML
     private void sellOrder() {
         String amount = amountTextField.getText();
         int hash_password = Password2Hash.hashPassword(passwordField.getText().trim());
         String operationInfo;
         boolean withdraw = false;
-
         // Special case: "-1"
         if ("-1".equals(amount)) {
             // withdraw the existing order
@@ -33,25 +27,24 @@ public class CryptoOrderController {
         } else {
             operationInfo = "pushOrder:" + UserObj.username + " " + "Sale " + amount + " " + hash_password;
         }
-
         double amountVal = Double.parseDouble(amount);
 
         // Check if amount is invalid
         if (!withdraw && amountVal <= 0) {
-            ShowAlert.Error("Invalid Amount", "Amount must be greater than zero.");
+            ShowAlert.Error("Montant invalide", "Le montant doit être supérieur à zéro.");
             return;
         }
 
         // Check if amount is greater than available currency
         if (!withdraw && amountVal > UserObj.account.investment.coins) {
-            ShowAlert.Error("Not Enough Coins", "Insufficient coins to place the sale order.");
+            ShowAlert.Error("Pas assez de Coins", "Nombre de coins insuffisant pour les mettre en vente.");
             return;
         }
 
         // Check if the user has already ordered
         if (GlobalObj.saleCoins.containsKey(UserObj.username)) {
             if (!withdraw) {
-                ShowAlert.Error("Already Ordered", "You have already placed a sale order.");
+                ShowAlert.Error("Offre déjà formulée", "Vous avez déjà effectué une mise en vente.");
                 return;
             } else {
                 amountVal = GlobalObj.saleCoins.get(UserObj.username);
@@ -59,29 +52,27 @@ public class CryptoOrderController {
         }
 
         if (!GlobalObj.saleCoins.containsKey(UserObj.username) && withdraw) {
-            ShowAlert.Error("Not yet Ordered", "You have no sale order to withdraw.");
+            ShowAlert.Error("Pas d'offre formulée", "Vous n'avez pas effectué de mise en vente.");
             return;
         }
 
         JavaClient client = new JavaClient();
         String resInfo = client.sendAndReceive(operationInfo);
         client.close();
-
         if ("1".equals(resInfo)) {
             if (withdraw) {
                 UserObj.account.investment.coins -= (float) amountVal;
                 GlobalObj.saleCoins.remove(UserObj.username);
-                ShowAlert.Information("Sale Order Withdrawn", "Sale order has been successfully withdrawn.");
+                ShowAlert.Information("Offre retirée de la vente", "L'offre mise en vente a été retirée avec succès.");
             } else {
                 UserObj.account.investment.coins += (float) amountVal;
                 GlobalObj.saleCoins.put(UserObj.username, (float) amountVal);
-                ShowAlert.Information("Sale Order Placed", "Sale order has been successfully placed.");
+                ShowAlert.Information("Offre mise en vente", "L'offre a été mise en vente avec succès.");
             }
         } else {
-            ShowAlert.Error("Sale Order Failed", "Failed to place the sale order. Please try again.");
+            ShowAlert.Error("Echec de la mise en vente", "Échec de la mise en vente de votre offre. Veuillez réessayer.");
         }
     }
-
 
     @FXML
     private void buyOrder() {
@@ -97,17 +88,16 @@ public class CryptoOrderController {
         } else {
             operationInfo = "pushOrder:" + UserObj.username + " " + "Purchase " + amount + " " + hash_password;
         }
-
         double amountVal = Double.parseDouble(amount);
         if(!withdraw) {
             if (amountVal <= 0) {
-                ShowAlert.Error("Invalid Amount", "Amount must be greater than zero.");
+                ShowAlert.Error("Montant invalide", "Le montant doit être supérieur à zéro.");
                 return;
             }
 
             // Check if amount is greater than available currency
             if (amountVal > UserObj.account.investment.coins) {
-                ShowAlert.Error("Not Enough Currency", "Insufficient coins to place the order.");
+                ShowAlert.Error("Pas assez d'argent courrant", "Pas assez de coins pour réaliser l'achat.");
                 return;
             }
         }
@@ -115,37 +105,34 @@ public class CryptoOrderController {
         // Check if the user has already ordered
         if (GlobalObj.buyCoins.containsKey(UserObj.username)) {
             if(!withdraw){
-                ShowAlert.Error("Already Ordered", "You have already placed an order.");
+                ShowAlert.Error("Achat déjà réalisé", "Vous avez déjà réalisé l'achat.");
                 return;
             } else {
                 amountVal = GlobalObj.buyCoins.get(UserObj.username);
             }
-
         }
 
         if (!GlobalObj.buyCoins.containsKey(UserObj.username) && withdraw) {
-            ShowAlert.Error("Not yet Ordered", "You have no order to withdraw.");
+            ShowAlert.Error("Achat non effectué", "Vous ne pouvez pas réaliser de retrait.");
             return;
         }
 
         JavaClient client = new JavaClient();
         String resInfo = client.sendAndReceive(operationInfo);
         client.close();
-
         if ("1".equals(resInfo)) {
             if(withdraw){
                 UserObj.account.investment.coins += (float) amountVal;
                 GlobalObj.buyCoins.remove(UserObj.username);
-                ShowAlert.Information("Purchase Order Withdrawn", "Purchase order has been successfully withdrawn.");
+                ShowAlert.Information("Achat retiré", "L'achat a été retirée avec succès.");
 
             } else {
                 UserObj.account.investment.coins -= (float) amountVal;
                 GlobalObj.buyCoins.put(UserObj.username, (float) amountVal);
-                ShowAlert.Information("Purchase Order Placed", "Purchase order has been successfully placed.");
+                ShowAlert.Information("Achat effectué", "L'achat a été effectué avec succès.");
             }
         } else {
-            ShowAlert.Error("Purchase Order Failed", "Failed to place the purchase order. Please try again.");
+            ShowAlert.Error("Echec de l'achat", "Échec de l'achat. Veuillez réessayer");
         }
     }
 }
-
